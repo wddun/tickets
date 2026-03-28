7c134358d4166ac8f91519c44a7d094f3702c293
William Dunning <willdunning01@gmail.com>
2026-03-28 15:06:26 -0400

    Add support page and update privacy policy

--- Ticket Check In/Ticket Check In/Ticket_Check_InApp.swift
+++ Ticket Check In/Ticket Check In/Ticket_Check_InApp.swift
@@ -6,9 +6,32 @@
 //
 
 import SwiftUI
+import UIKit
 
 @main
 struct Ticket_Check_InApp: App {
+    init() {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor.systemBackground
+
+        let normalColor = UIColor.secondaryLabel
+        let selectedColor = UIColor(named: "AccentColor") ?? UIColor.systemBlue
+        appearance.stackedLayoutAppearance.normal.iconColor = normalColor
+        appearance.stackedLayoutAppearance.normal.titleTextAttributes = [
+            .foregroundColor: normalColor
+        ]
+        appearance.stackedLayoutAppearance.selected.iconColor = selectedColor
+        appearance.stackedLayoutAppearance.selected.titleTextAttributes = [
+            .foregroundColor: selectedColor
+        ]
+
+        UITabBar.appearance().standardAppearance = appearance
+        if #available(iOS 15.0, *) {
+            UITabBar.appearance().scrollEdgeAppearance = appearance
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
